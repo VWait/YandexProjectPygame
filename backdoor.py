58a6cf3fdb20742c3837973e32a6546cd282eef4
VWait <95030049+VWait@users.noreply.github.com>
2022-01-17 21:08:15 +0500

    всё в классах

--- backdoor.py
+++ backdoor.py
@@ -2,12 +2,22 @@ from random import shuffle
 
 
 class Card:
+    def __init__(self, game, value):
+        self.game = game
+        self.value = value
+
     def move(self):
         pass
 
 
 class Card1(Card):
-    pass
+    def move(self):
+        selected_player_i = self.game.choice_other_player(self.game.count_players, self.game.player_i)
+        selected_card = self.game.choice_card_value()
+        if self.game.players[selected_player_i].card.value == selected_card:
+            print(f'Player {selected_player_i + 1} lose')
+            del self.game.players[selected_player_i]
+            self.game.count_players -= 1
 
 
 class Card2(Card):
@@ -38,18 +48,45 @@ class Card8(Card):
     pass
 
 
-values_cards = {1: Card1, 2: Card2, 3: Card3, 4: Card4,
-                5: Card5, 6: Card6, 7: Card7, 8: Card8}
+class Game:
+    values_cards = {1: Card1, 2: Card2, 3: Card3, 4: Card4,
+                    5: Card5, 6: Card6, 7: Card7, 8: Card8}
+
+    def __init__(self):
+        self.deck = Deck(self, [1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8])
+        self.count_players = 3
+        self.players = [Player(self.deck.get_and_pop_card()) for i in range(self.count_players)]
+        self.player_i = 0
+
+    def game_cycle(self):
+        while len(self.deck.deck) > 0:
+            print([p.card.value for p in self.players])
+            self.players[self.player_i].move(self.deck)
+            self.player_i = (self.player_i + 1) % self.count_players
+
+    def choice_player(self, cnt_ps):
+        """return player number"""
+        return int(input('Pick one player: ' + ' '.join([str(i) for i in range(1, cnt_ps + 1)]))) - 1
+
+    def choice_other_player(self, cnt_ps, p_i):
+        """return player number"""
+        return int(input('Pick one player: ' + ' '.join([str(i) for i in range(1, cnt_ps + 1) if i - 1 != p_i]))) - 1
+
+    def choice_card_value(self):
+        """return card number"""
+        return int(input('Pick one card: ' + ' '.join([str(i) for i in range(2, 9)])))
 
 
 class Deck:
-    def __init__(self, cards):
+    def __init__(self, game, cards):
+        self.game = game
+        self.values_cards = self.game.values_cards
         self.cards = cards
         self.deck = []
         self.fill_and_shuffle_deck()
 
     def save_card(self, value):
-        self.deck.append(values_cards[value])
+        self.deck.append(self.values_cards[value](self.game, value))
 
     def fill_and_shuffle_deck(self):
         [self.save_card(value) for value in self.cards]
@@ -64,20 +101,14 @@ class Player:
         self.card = start_card
 
     def move(self, dk):
-        self.card = [self.card, dk.get_and_pop_card()] # player take one more card
+        self.card = [self.card, dk.get_and_pop_card()]  # player take one more card
         self.choice_of_card()
 
     def choice_of_card(self):
-        choice = int(input('0 или 1'))
-        self.card[choice].move() # discard one card
-        self.card = self.card[(choice + 1) % 2] # save other card
+        choice = int(input(f'0({self.card[0].value}) или 1({self.card[1].value})'))
+        self.card[choice].move()  # discard one card
+        self.card = self.card[(choice + 1) % 2]  # save other card
 
 
-deck = Deck([1, 1, 1, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 8])
-count_players = 3
-players = [Player(deck.get_and_pop_card()) for i in range(count_players)]
-player_i = 0
-
-
-while True:
-    break
+game = Game()
+game.game_cycle()
